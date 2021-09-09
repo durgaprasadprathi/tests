7bf0d80429b6c8103dce44dcaf6ca1c97284e652
abshashank <absashank@gmail.com>
2021-09-09 16:21:00 +0530

    w

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "product" {
+
+}
+variable "environment" {
+
+}
+variable "vpc_cidr" {
+
+}
+variable "tenancy" {
+
+}
+variable "public_subnets" {
+
+}
+variable "private_subnets" {
+
+}
+variable "availability_zones" {
+    
+}
